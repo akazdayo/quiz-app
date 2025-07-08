56230e9b1499529a2daaf46c76c715aba8361948
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-07-08 11:23:25 +0900

    Cloudflareを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import solidJs from '@astrojs/solid-js';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare(),
 });
